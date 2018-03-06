63274a24649af36e60f98951d7a48b56931cf413
kongdev <nopaket@gmail.com>
2018-03-06 20:19:22 +0700

    ok

---
+++ index.js
@@ -0,0 +1,30 @@
+//const express = require('express')
+
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+app.get('/', function (req, res,next) {
+    
+    res.send('Hello World!')
+})
+
+
+app.use((err, req, res, next) => {
+    //console.log("Request data:");
+    res.status(err.status || 500);
+    res.json({clientData: { status: false}})
+
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
